b802a0c341df07c43c0cd65f41999aa930ee0f2a
Omar <w0475798@campus.nscc.ca>
2024-04-02 10:25:43 -0300

    positioning

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,86 +3,87 @@ import '../utils.dart';
 
 class HomeScreen extends StatelessWidget { 
 
-    @override 
-    Widget build(BuildContext context ) { 
-        return Scaffold(
-
-            backgroundColor: const Color(0xff494F55),
-            body:
-            Padding( 
-                padding:const EdgeInsets.only(left:20.0, right:20, top:50),
-                child: SingleChildScrollView(
-                physics: ScrollPhysics(),
-                child:Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                        Text("Crypto Market", style: textStyle(24, Colors.white, FontWeight.bold), 
-                        
-                        ),                        //text
-
-                        ListView.builder(  
-                            shrinkWrap: true,
-                            physics: NeverScrollableScrollPhysics(),
-                            itemCount: 10, 
-                            itemBuilder: (context, index) {
-                                
-                                return Padding( 
-                                    padding: const EdgeInsets.only(bottom: 15.0),
-                                    child: Container(
-                                width:MediaQuery.of(context).size.width,
-                                height: 60,
-                                child: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
-                                    children:[
-                                        Container(
-                                    width:60,
-                                    height:60,
-                                    decoration:BoxDecoration(
-                                           color:Colors.grey[700],
-                                           borderRadius:BorderRadius.circular(15),
-                                            boxShadow: [
-                                                BoxShadow(
-                                                    color:Colors.grey[700]!,
-                                                    offset: const Offset(4, 4),
-                                                    blurRadius: 5,
-                                             )//boxShadow
-                                            ]
-
-                                    ),//boxDecoration
-                                    child: Padding(
-                                        padding: const EdgeInsets.all(10),
-                                        child: Image.network('https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/1200px-Bitcoin.svg.png')
-                                    ), //padding
-                                        ),//Container
-                                        const SizedBox(width:20, ),
-                                        Column (
-                                            crossAxisAlignment: CrossAxisAlignment.start,
-                                        children:[
-                                            Text("\$$ 2", 
-                                            style: textStyle
-                                            (18, Colors.white, FontWeight.w600),
-                                            ),//text
-                                            Text("10 %", 
-                                            style: textStyle
-                                            (18, Colors.grey, FontWeight.w600),
-                                            ),//text
-
-                                        ],
-                                        )//column
-                                    ],
-                                )//row
-                                
-                                )//container
-                                );//padding
-                            },
-                             )//Listview.builder
-                    ],
-                ),//column
-
-            ),//singlechildscrollview
-
-        )); //padding /scaffold
-        
-    }
+  @override 
+  Widget build(BuildContext context ) { 
+    return Scaffold(
+      backgroundColor: const Color(0xff494F55),
+      body: Padding( 
+        padding: const EdgeInsets.only(left:20.0, right:20, top:50),
+        child: SingleChildScrollView(
+          physics: ScrollPhysics(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Crypto Market", style: textStyle(24, Colors.white, FontWeight.bold),), //text
+              ListView.builder(  
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: 10, 
+                itemBuilder: (context, index) {
+                  return Padding( 
+                    padding: const EdgeInsets.only(bottom: 15.0),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 60,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children:[
+                          Row(
+                            children: [
+                              Container(
+                                width:60,
+                                height:60,
+                                decoration: BoxDecoration( // Removed extra positional arguments
+                                  color:Colors.grey[700],
+                                  borderRadius:BorderRadius.circular(15),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color:Colors.grey[700]!,
+                                      offset: const Offset(4, 4),
+                                      blurRadius: 5,
+                                    ),//boxShadow
+                                  ],
+                                ),//boxDecoration
+                                child: Padding(
+                                  padding: const EdgeInsets.all(10),
+                                  child: Image.network('https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/1200px-Bitcoin.svg.png'),
+                                ), //padding
+                              ),//Container
+                              const SizedBox(width:20), // Moved this line to be outside Container
+                              Column (
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children:[
+                                  Text("Bitcoin", 
+                                    style: textStyle(18, Colors.white, FontWeight.w600),
+                                  ),//text
+                                  Text("10 %", 
+                                    style: textStyle(18, Colors.grey, FontWeight.w600),
+                                  ),//text
+                                ],
+                              ),//column
+                            ],
+                          ), // Add missing closing parenthesis here
+                          Column (
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children:[
+                              Text("\$ 200", 
+                                style: textStyle(18, Colors.white, FontWeight.w600),
+                              ),//text
+                              Text("BI", 
+                                style: textStyle(18, Colors.grey, FontWeight.w600),
+                              ),//text
+                            ],
+                          ), //column
+                        ],
+                      )//row
+                    )//container
+                  );//padding
+                },
+              ),//Listview.builder
+            ],
+          ),//column
+        ),//singlechildscrollview
+      ), //padding
+    ); //scaffold
+  }
 }
