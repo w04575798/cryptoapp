02cd80457353b2a1a246b24c670af4dd8246f60b
Omar <w0475798@campus.nscc.ca>
2024-04-07 03:01:46 -0300

    initialisation

--- lib/controllers/coincontroller.dart
+++ lib/controllers/coincontroller.dart
@@ -4,14 +4,27 @@ import 'package:http/http.dart' as http;
 
 class CoinController extends GetxController { 
     RxList<Coin> coinstList =<Coin>[].obs;
-
+    RxBool isLoading = true.obs;
+    @override
+    void onInit(){ 
+        super.onInit();
+        fetchCoins();
+    }
     fetchCoins()async{
-        var response = await http.get(
+        try { 
+                            isLoading(true);
+
+             var response = await http.get(
             Uri.parse('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en');
         )
         List<Coin> coins = coinFromJson(response.body);
 
         coinsList.value = coins;
+
+        } finally { 
+                isLoading(false);
+        }
+       
     }
 }
 
